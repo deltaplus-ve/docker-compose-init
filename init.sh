dbcd62ecfc85fd7e99ead2eceed5eee669b4c71e
Alberto Güerere <aguerere@users.noreply.github.com>
2016-05-03 23:21:03 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 rm -rf docker-compose
 git clone https://github.com/deltaplus-ve/docker-compose.git
 cd docker-compose
-chmod +x init.sh up.sh down.sh
+chmod +x init.sh up.sh down.sh reload.sh
 ./init.sh
 ./up.sh
