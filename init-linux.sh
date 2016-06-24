0602ebbea5c1f47b7d7a552e327e9f22d7451ac9
Beto <albertoguerere@gmail.com>
2016-06-24 06:56:15 -0400

    Agrega init para Linux

---
+++ init-linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf docker-compose
+git clone https://github.com/deltaplus-ve/docker-compose.git
+cd docker-compose
+chmod +x init.sh up.sh down.sh reload.sh
+./init.sh
+./up-linux.sh
