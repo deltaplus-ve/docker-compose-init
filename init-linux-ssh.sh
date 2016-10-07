8028516d6f1629b55a9e9b680351e43f582da75d
Alberto Güerere <aguerere@users.noreply.github.com>
2016-10-07 12:08:03 -0400

    Checkout swagger-graphql branch

--- init-linux-ssh.sh
+++ init-linux-ssh.sh
@@ -3,6 +3,7 @@
 rm -rf docker-compose
 git clone git@github.com:deltaplus-ve/docker-compose.git
 cd docker-compose
+git checkout swagger-graphql
 chmod +x init-ssh.sh up.sh down.sh reload.sh
 ./init-ssh.sh
 ./up-linux.sh
