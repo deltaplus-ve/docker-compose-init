4f63b881efdbd57324e4f5b53511d342d44eb44c
Alberto Güerere <alberto.guerere@techaid.co>
2016-10-12 09:30:16 -0400

    Update macOS init script.

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 rm -rf docker-compose
 git clone https://github.com/deltaplus-ve/docker-compose.git
 cd docker-compose
+git checkout swagger-graphql
 chmod +x init.sh up.sh down.sh reload.sh
 ./init.sh
 ./up.sh
